5400b28709e2cacae08959d9b7edb8b9c233c2bc
Dmitry <leshuk_03@mail.ru>
2023-09-06 19:05:38 +0300

    added some tasks

--- app/src/main/java/by/bstu/fit/lab1/MainActivity.java
+++ app/src/main/java/by/bstu/fit/lab1/MainActivity.java
@@ -1,7 +1,10 @@
 package by.bstu.fit.lab1;
 
+import android.util.Log;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import by.bstu.fit.lab1.text.TextFunction;
 
 public class MainActivity extends AppCompatActivity {
 //TODO Add new method
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayText();
+        for (int count = 0; count < 10; count++) {
+            Log.d("MainActibity"," counter=" + count);
+        }
+    }
+
+    private void displayText () {
+        TextFunction tf = new TextFunction();
+        TextView nt = findViewById(R.id.newtest);
+        nt.setText(tf.getValue());
     }
 }
